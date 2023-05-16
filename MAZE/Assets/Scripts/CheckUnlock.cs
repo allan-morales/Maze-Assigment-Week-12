40bdfd7830168c33989eb27e4097868da0920aab
allan-morales <allanmorales.apps@gmail.com>
2023-05-16 14:57:31 -0400

    Maze Assignment to GitHub

---
+++ MAZE/Assets/Scripts/CheckUnlock.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+
+public class CheckUnlock : MonoBehaviour
+{
+    public GameObject door = null;
+    void Start()
+    {
+        door.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Key"))
+        {
+            // unlock!
+            if (door.activeInHierarchy)
+                door.SetActive(false);
+
+        }
+
+    }
+}
---
+++ MAZE/Assets/Scripts/CheckWin.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+    public GameObject winMessage;
+    void Start()
+    {
+        winMessage.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            // we won!
+            if (!winMessage.activeInHierarchy)
+                winMessage.SetActive(true);
+
+        }
+
+
+    }
+
+}
